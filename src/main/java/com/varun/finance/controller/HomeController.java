a6f167ea16930aa3a284cb6e642fde1f7d7978b8
VARUN <ab@.com>
2026-02-14 22:39:43 -0500

    "feat(web): redirect / to payment page" \ -m "Add HomeController to send / -> /payment.html without adding static index.html."

---
+++ src/main/java/com/varun/finance/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.varun.finance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/payment.html";
+    }
+}
